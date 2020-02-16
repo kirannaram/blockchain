7b9933706b36144d062b621cd0bf0dd6e0115265
kiran kumar naram <kiran.naram@gmail.com>
2020-02-17 01:03:32 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Container from './components/container'
+import store from "./redux/store"
+import { Provider } from 'react-redux';
+
+import "./assets/css/styles.css";
+
+ReactDOM.render(<Provider store={store}><Container /></Provider>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
